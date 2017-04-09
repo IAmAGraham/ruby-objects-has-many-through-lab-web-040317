ceb66f86e1318bfd94d63b18d95af1ea59823036
github username <github email address>
2017-04-09 19:30:17 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+
+ class Song
+ 
+   attr_reader :title
+   attr_accessor :genre, :artist
+ 
+   def initialize(title, genre)
+     @title = title
+     @genre = genre
+     @artist = artist
+     genre.add_song(self)
+   end
+ 
+ end
